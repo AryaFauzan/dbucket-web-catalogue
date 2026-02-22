fb3a8645551a4f5278313874902e87a9a4514cf6
Arya Cahya Fauzan <aryafauzan191003@gmail.com>
2026-02-23 03:22:35 +0700

    feat: menambahkan vercel web analytics

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './assets/style.css' // Pastikan CSS Tailwind v4 tetap diimpor
+import { inject } from '@vercel/analytics'
 import App from './App.vue'
 import router from './router/router' // Impor router
 
@@ -7,3 +8,5 @@ const app = createApp(App)
 
 app.use(router) // Gunakan router
 app.mount('#app')
+
+inject() // Inisialisasi Vercel Analytics
